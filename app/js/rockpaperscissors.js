05fa463f09c2520b9bba31f450d6161425228fe3
amansn <amanjeet@gmail.com>
2016-02-15 19:36:32 -0500

    Finished RPS Project

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -42,24 +42,31 @@ function getWinner(playerMove,computerMove) {
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     if (playerMove === computerMove) {
         winner = "tie";
+        console.log("It was a tie!");
     } else {
         if (playerMove === "rock") {
             if (computerMove === "scissors") {
                 winner = "player";
+                console.log("You won!");
             } else {
                 winner = "computer";
+                console.log("You lost :(");
             }
         } else if (playerMove === "scissors") {
             if (computerMove === "rock") {
                 winner = "computer";
+                console.log("You lost :(");
             } else {
                 winner = "player";
+                console.log("You won!");
             }
         } else {
             if (computerMove === "scissors") {
                 winner = "computer";
+                console.log("You lost :(");
             } else {
                 winner = "player";
+                console.log("You won!");
             }
         }
     }
@@ -71,7 +78,22 @@ function playToFive() {
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    while (playerWins < 5 && computerWins < 5) {
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        winner = getWinner(playerMove, computerMove);
+        if (winner === "player") {
+            playerWins = playerWins + 1;
+        } else if (winner === "computer") {
+            computerWins = computerWins + 1;
+        }
+    }
+    if (playerWins === 5) {
+        console.log("Congrats! You won best of 5!");
+    } else {
+        console.log("Computer won best of 5!");
+    }
     return [playerWins, computerWins];
 }
+
 
